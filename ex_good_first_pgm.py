1a58badce0088d4ec7c67fac485727ef3f666e93
gauravmhkd25 <gauravmhkd25@gmail.com>
2018-09-15 11:46:56 +0530

    this

---
+++ ex_good_first_pgm.py
@@ -0,0 +1,7 @@
+print("Hello world")
+print("hello again")
+print("I like typing this")
+print("This is fun.")
+print('yay! printing')
+print("I'd much rather you 'now'")
+#print('I "said" do not touch it')
