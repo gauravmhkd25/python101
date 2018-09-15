31f721b63132ce4e497c2959fb6e2204190e114b
gauravmhkd25 <gauravmhkd25@gmail.com>
2018-09-15 20:54:25 +0530

    use of pound

---
+++ hw_2_pound.py
@@ -0,0 +1,6 @@
+print("this shows the use of # (pound)")
+print("# is commonly used for commenting in python code to make it readable")
+print("when any line starts with #, it is not executed and hence becomes a comment, ignored by the interpreter")
+#print("this line will not be seen")
+#the above line is a comment
+print('end')
