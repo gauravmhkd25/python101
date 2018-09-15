c365d4a9e37474510f396b3eb092b77c9ab41cec
gauravmhkd25 <gauravmhkd25@gmail.com>
2018-09-15 20:44:38 +0530

    printing

---
+++ hw_1_print.py
@@ -0,0 +1,12 @@
+print("This is the example for printing in python")
+print('writing a text in single quotes is also acceptable')
+#print("this text will "NOT" excute")
+#print('this will also 'NOT' execute')
+print(" double and 'sigle' quotes work the same way")
+print('but they should be "used" inclusively')
+print('either \' \'  should be inside', end=' ')
+print(' " " ')
+print("or")
+print('vice versa')
+~
+
