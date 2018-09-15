359891b9422af8f9128980eafef705a8e291d3da
gauravmhkd25 <gauravmhkd25@gmail.com>
2018-09-15 21:25:27 +0530

    root of a number

---
+++ hw_3.2_sqrt.py
@@ -0,0 +1,7 @@
+print("this code computes square root(sqrt) of a number")
+print("a number raised to 1/2 will result in sqrt of the number")
+print("ex: 49**( 1/2 )")
+print("result is= ", 49**(1/2))
+print("similarly a number raised to 1/n where n=1,2,3.... is its nth root")
+print("cube root of 27 is 27**( 1 / 3)= ",27**( 1/3))
+
