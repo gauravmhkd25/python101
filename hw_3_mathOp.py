7461e158c0bb54a8b9e2f74f1e46fa60f2617dd7
gauravmhkd25 <gauravmhkd25@gmail.com>
2018-09-15 21:14:54 +0530

    math opp

---
+++ hw_3_mathOp.py
@@ -0,0 +1,21 @@
+print("This py code includes example for math operators")
+print("different math operators are +, -, *, /, **")
+print("let a=5 and b=6")
+a=5
+b=6
+print("then the following are some possible expressions: ")
+print("Addition: a + b = ",a+b)
+print("Sub: a - b = ",a-b)
+print("Multiply: a * b = ",a*b)
+print("divide: a / b = ",a/b)
+print("power: a ** b =",a**b)
+print("if multiple operators are used in the expression, for ex: a+b*c-a")
+print("the above expression is solved using precedence")
+print("order of precedence L->R is: ( ), **, * or /, + or -")
+print("let c=7, d=8, e=0")
+c=7
+d=8
+e=0
+print("this expression: ( a**e + ( c - d ) ) - b + d * e =",( a**e + ( c - d ) ) - b + d * e )
+print("is solved by the above mentioned order i.e. inner ( ) -> outer ( ) -> * -> + or -")
+
