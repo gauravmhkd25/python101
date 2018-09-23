40a13e5bd98b616a0aa4db47db867abd05bf457f
gauravmhkd25 <gauravmhkd25@gmail.com>
2018-09-23 14:21:13 +0530

    hw_5
    ESCAPE SEQUENCE IN PYTHON

--- Home_Work/hw_5_printing_in_singleLine.py
+++ Home_Work/hw_5_printing_in_singleLine.py
@@ -1,28 +1,47 @@
-print("*"*35)
-print(" "*5,"Concatenating strings")
-print("*"*35)
-s1 = 'G'
-s2 = 'a'
-s3 = 'u'
-s4 = 'r'
-s5 = 'a'
-s6 = 'v'
+print("*"*62)
+print(" "*5,"Concatenating strings",end = '\f')
+print("with escape sequence")
+print("*"*62)
+s1 = 'Gaurav'
 s7 = 'M'
 s8 = 'h'
 s9 = 'k'
 s10 = 'd'
 
-print(s1 + s2 + s3 + s4 + s5 + s6, end = ' ')
+print(s1, end = ' ')
 print(s7 + s8 + s9 + s10)
 
-print("\033[2;37;40m #The above print statement is with 'end = ' ' '\033[0;37;40m")
+print("\033[2;37;40m #The above print statement is with 'end = ' ' normal space'\033[0;37;40m\n")
 
 
-print(s1 + s2 + s3 + s4 + s5 + s6, end = '\n')
+print(s1 , end = '\n')
 print(s7 + s8 + s9 + s10)
+print('\033[2;37;40m #The above print statement is with "end =  \\n " newLine\033[0;37;40m\n')
 
-print('\033[2;37;40m #The above print statement is without "end = ' ' "\033[0;37;40m')
+print(s1, end = '\t')
+print(s7 + s8 + s9 + s10)
+print('\033[2;37;40m #The above print statement is with "end = \\t " tab\033[0;37;40m\n')
 
+print(s1, end = '\a')
+print(s7 + s8 + s9 + s10)
+print('\033[2;37;40m #The above print statement is with "end = \\a " bell\033[0;37;40m\n')
 
+print(s1, end = '\b')
+print(s7 + s8 + s9 + s10)
+print('\033[2;37;40m #The above print statement is with "end = \\b " backSpace\033[0;37;40m\n')
 
+print(s1, end = '\f')
+print(s7 + s8 + s9 + s10)
+print('\033[2;37;40m #The above print statement is with "end = \\f " formfeed\033[0;37;40m\n')
 
+print(s1, end = '\r')
+print(s7 + s8 + s9 + s10)
+print('\033[2;37;40m #The above print statement is with "end = \\r " carraige return\033[0;37;40m\n')
+
+print(s1, end = '\v')
+print(s7 + s8 + s9 + s10)
+print('\033[2;37;40m #The above print statement is with "end = \\t " vertical tab\033[0;37;40m\n')
+
+print(s1, end = '\'')
+print(s7 + s8 + s9 + s10)
+print('\033[2;37;40m #The above print statement is with "end = \\' " vertical tab\033[0;37;40m\n')
