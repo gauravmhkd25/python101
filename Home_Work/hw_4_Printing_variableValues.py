b7f8fff08b2310dac722f2433d807cabbbab75c2
gauravmhkd25 <gauravmhkd25@gmail.com>
2018-09-23 14:41:10 +0530

    Update hw_4_Printing_variableValues.py
    a simple math program with printing the result.

--- Home_Work/hw_4_Printing_variableValues.py
+++ Home_Work/hw_4_Printing_variableValues.py
@@ -12,9 +12,19 @@ carpool = driven * space
 avg_passenger_per_car = passengers/driven
 
 print("Number of available cars is ",cars)
-print("to drive them,",drivers," drivers arevailable.")
-print("If the number of drivers are less, then",noDrive,"cars will not be driven.")
-print("Today",carpool,"people can be transported.")
-print("There are", passengers,"passengers to carpool today.")
-print("There will be atleast",avg_passenger_per_car,"in each car.")
+#output: Number of available cars is  50
 
+print("to drive them,",drivers," drivers arevailable.")
+#output: to drive them, 30  drivers arevailable.
+
+print("If the number of drivers are less, then",noDrive,"cars will not be driven.")
+#output: If the number of drivers are less, then 20 cars will not be driven.
+
+print("Today",carpool,"people can be transported.")
+#output: Today 120.0 people can be transported.
+
+print("There are", passengers,"passengers to carpool today.")
+#output: There are 90 passengers to carpool today.
+
+print("There will be atleast",avg_passenger_per_car," passengers in each car.")
+#output: There will be atleast 3.0 passengers in each car.
