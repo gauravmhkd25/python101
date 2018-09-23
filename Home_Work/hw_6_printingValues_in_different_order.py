7618e2e2a1c98412b24edf0e420a6a1055b581a8
gauravmhkd25 <gauravmhkd25@gmail.com>
2018-09-23 14:46:39 +0530

    formatted strings

--- Home_Work/hw_6_printingValues_in_different_order.py
+++ Home_Work/hw_6_printingValues_in_different_order.py
@@ -1,8 +1,17 @@
-order = "{}, {} and {}".format('Gaurav','Kumar','Mahakud')
+order = "{} {} {}".format('Gaurav','Kumar','Mahakud')
+#order is tha variable which stores the formatted order
+
 print('The name is: ',end='\n ')
 print(order)
+#output: Gaurav kumar Mahakud
+#the same formatted order is printed
 
-print('Printing Last name first {2} {0} {1}'.format('Gaurav','kumar','Mahakud'),end='\n')
+print('Printing Last name first: {2} {0} {1}'.format('Gaurav','kumar','Mahakud'),end='\n')
+#output: Printing Last name first: Mahakud Gaurav kumar
+
 print("Assigning keys to strings--a='Gaurav', b='kumar', c='Mahakud'")
+#output: Assigning keys to strings--a='Gaurav', b='kumar', c='Mahakud'
+
 key_order = "{a} {b} {c}".format(a='Gaurav',b='kumar',c='Mahakud')
 print(key_order)
+#output: Gaurav kumar Mahakud
