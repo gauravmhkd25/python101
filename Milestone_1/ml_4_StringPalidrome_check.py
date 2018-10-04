6a2c5c68c150b00539de2e3d4340d13c4346c355
gauravmhkd25 <gauravmhkd25@gmail.com>
2018-10-04 13:16:51 +0530

    Update ml_4_StringPalidrome_check.py

--- Milestone_1/ml_4_StringPalidrome_check.py
+++ Milestone_1/ml_4_StringPalidrome_check.py
@@ -1,19 +1,62 @@
 print("***********************************",'\n')
 print(" "*10+"\033[1;34;40mPalindrome Test\033[0m\n")
 print("***********************************")
+#header with allignment and coloured text
+
 usr = input("Enter a string:  ")
+#accepting user string
+
 rev = ''
+#initailising a rev variable(to store reverse) to null
+
 flag = False
+#flag to indicate if the string is palindrome, initialised to Flase
 
 for i in range(len(usr)-1,-1,-1):
     rev += usr[i]
+#loop to obtain reverse of the user string
 
 print("Reverse is: ",rev,end=' ')
+#printing reversed string
 
 for i in range(len(usr)):
     if rev[i] == usr[i]:
-        flag = True
-    else: flag = False
+        flag = True #when the comparison is a hit, flag is set to true
+    else: flag = False #else it is false
+#comparing actual string to the reversed string
 
-if flag == False: print("which is \033[1;91;40mnot a palindrome\033[0m")
-else: print("which is a \033[1;92;40mpalindrome\033[0m")
+if flag == False: print("which is \033[1;91;40mnot a palindrome\033[0m") #printing not a palindrome if flag is false
+else: print("which is a \033[1;92;40mpalindrome\033[0m") #printing: it is a palindrome if flag is true
+
+    
+#result:✔  
+#$ py ml_4_StringPalidrome_check.py
+#***********************************
+
+#          Palindrome Test
+
+#***********************************
+#Enter a string:  Nurses run
+#Reverse is:  nur sesrun which is a palindrome
+
+#2
+#***********************************
+
+#          Palindrome Test
+
+#***********************************
+#Enter a string:  1010101
+#Reverse is:  1010101 which is a palindrome
+
+#3
+#$ py ml_4_StringPalidrome_check.py
+#***********************************
+
+#          Palindrome Test
+
+#***********************************
+#Enter a string:  oporop
+#Reverse is:  poropo which is not a palindrome
+
+
+
